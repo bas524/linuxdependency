79d9857a5c5993ef1cf210f43cfe0dfcf624e9b2
Alexander Bychuk <alexander.bychuk@kaspersky.com>
2020-01-22 16:33:19 +0300

    new : add install (useful for nemo file manager)

--- nautilus-python/extensions/linuxdependency.py
+++ nautilus-python/extensions/linuxdependency.py
@@ -1,4 +1,3 @@
-
 import os, os.path
 import urllib
 
@@ -6,10 +5,11 @@ from gi.repository import Nautilus, GObject, GConf
 
 QLDD_KEY = '/usr/bin/Qldd'
 
+
 class OpenQlddExtension(Nautilus.MenuProvider, GObject.GObject):
     def __init__(self):
         pass
-        
+
     def _open_qldd(self, file):
         filename = urllib.unquote(file.get_uri()[7:])
         qldd = QLDD_KEY
@@ -21,13 +21,13 @@ class OpenQlddExtension(Nautilus.MenuProvider, GObject.GObject):
     def get_file_items(self, window, files):
         if len(files) != 1:
             return
-        
+
         file = files[0]
         if file.is_directory() or file.get_uri_scheme() != 'file':
             return
-        
+
         item = Nautilus.MenuItem(name='NautilusPython::openqldd_file_item',
-                                 label='View dependecy' ,
-                                 tip='View dependecy of %s' % file.get_name())
+                                 label='View dependencies',
+                                 tip='View dependencies of %s' % file.get_name())
         item.connect('activate', self.menu_activate_cb, file)
         return item,
